b9706f855b882048152e95beb35f9472520ea09c
lnnguyen02 <lnnguyen02@gmail.com>
2022-03-06 05:25:40 -0500

    Added setUp() and readingInput().

--- TOS-java/src/TextSpeech.java
+++ TOS-java/src/TextSpeech.java
@@ -2,6 +2,7 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Locale;
 import javax.speech.Central;
 import javax.speech.synthesis.Synthesizer;
@@ -11,6 +12,8 @@ import java.util.Scanner;
 
 public class TextSpeech {
 
+	private static HashMap<String, String> responses = new HashMap<>();
+
 	/**
 	 * 
 	 * @param args
@@ -18,48 +21,36 @@ public class TextSpeech {
 	public static void main(String[] args) {
 
 		try {
-			// Set property as Kevin Dictionary
+			// Setting up the Text to Speech voice
 			System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
-
-			// Register Engine
 			Central.registerEngineCentral("com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");
-
-			// Create a Synthesizer
 			Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
-
-			// Allocate synthesizer
 			synthesizer.allocate();
-
-			// Resume Synthesizer
 			synthesizer.resume();
 
-			// Speaks the given text
-			// until the queue is empty.
-			//synthesizer.speakPlainText("Hello World!", null);
-			//synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+			// Setting up the responses for the chat bot
+			setUp();
+			readingInput(synthesizer);
 			
-			// Reads out the content of tempFile.txt
-			speak(synthesizer, "tempFile.txt");
-
 			// Deallocate the Synthesizer.
 			synthesizer.deallocate();
-		}
-
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Reads, line by line, the fileName.txt outloud.
+	 * Reads, line by line, the fileName.txt out loud.
 	 * 
-	 * @param synth A proper Synthesizer object
-	 * @param fileName A valid text file name that exists on the same level as the src folder
+	 * @param synth    A proper Synthesizer object
+	 * @param fileName A valid text file name that exists on the same level as the
+	 *                 src folder
 	 * 
 	 * @throws IllegalArgumentException
 	 * @throws InterruptedException
 	 */
-	public static void speak(Synthesizer synth, String fileName) throws IllegalArgumentException, InterruptedException {
+	public static void speakFile(Synthesizer synth, String fileName)
+			throws IllegalArgumentException, InterruptedException {
 		try {
 
 			Scanner fileReader = new Scanner(new File(fileName));
@@ -75,4 +66,39 @@ public class TextSpeech {
 		}
 
 	}
+
+	/**
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public static void setUp() throws FileNotFoundException {
+		Scanner fileReader = new Scanner(new File("text.txt"));
+		while (fileReader.hasNextLine()) {
+			responses.put(fileReader.nextLine(), fileReader.nextLine());
+		}
+	}
+
+	/**
+	 * 
+	 * @param synthesizer
+	 * @throws IllegalArgumentException
+	 * @throws InterruptedException
+	 */
+	public static void readingInput(Synthesizer synthesizer) throws IllegalArgumentException, InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		loop: while (true) {
+			String input = scanner.nextLine();
+
+			if (input.compareTo("exit") == 0)
+				break loop;
+
+			if (responses.get(input) != null) {
+				synthesizer.speakPlainText(responses.get(input), null);
+				synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+			}
+		}
+
+		scanner.close();
+	}
+	// EOF
 }
